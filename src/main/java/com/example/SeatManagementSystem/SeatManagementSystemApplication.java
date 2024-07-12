883f2846b22b03af24e7ffb0ca0fd7f9d1acf23a
Gaurav Kumar <gaurav.kumarsd29@gmail.com>
2024-07-12 09:45:50 +0530

    repo setup

--- src/main/java/com/example/SeatManagementSystem/SeatManagementSystemApplication.java
+++ src/main/java/com/example/SeatManagementSystem/SeatManagementSystemApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.SeatManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SeatManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SeatManagementSystemApplication.class, args);
 	}
 
 }
